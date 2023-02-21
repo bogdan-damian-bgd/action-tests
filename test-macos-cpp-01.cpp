ee9184d868a52fa33931e396d2cd51393d060643
bogdan-damian-bgd <108331162+bogdan-damian-bgd@users.noreply.github.com>
2023-02-21 14:28:25 +0100

    Update test-macos-cpp-01.cpp

--- test-macos-cpp-01.cpp
+++ test-macos-cpp-01.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 int main (){
+  
   // Map
   map<char,int> first;
   
@@ -18,14 +19,8 @@ int main (){
 
    // Time
    time_t now = time(0);
-   
    char* dt = ctime(&now);
-
    cout << "The local date and time is: " << dt << endl;
-
-   tm *gmtm = gmtime(&now);
-   dt = asctime(gmtm);
-   cout << "The UTC date and time is:"<< dt << endl;
    
 
   return 0;
